b419e3bfad3decc5c826ea024b75cf7e7936de75
0xrockey <rockey.rocket.dev@gmail.com>
2024-04-22 06:57:19 +0200

    add command

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// use  Package underscore 
 var _ = require('underscore')
 var result = _.contains([1,2,3] , 5)
 console.log(result)
+
+
+
+// Semantic Version 
+// Example 
+//  underscore :"^1,8,3"      Major,Minor,Patch 
+
+// Useful NPM commands are:
+// Install a package
+// npm i <packageName>
+// Install a specific version of a package
+// npm i <packageName>@<version>
+// Install a package as a development dependency
+// npm i <packageName> —save-dev
+// Uninstall a package
+// npm un <packageName>
+// List installed packages
+// npm list —depth=0
+// View outdated packages
+// npm outdated
+// Update packages
+// npm update
